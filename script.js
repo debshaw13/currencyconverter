4571dfc9fbcf3d26af7a01d9c548ffb3adc8c01b
Deb Shaw <debashishshaw@gmail.com>
2020-11-22 21:15:19 +0100

    react set up

---
+++ script.js
@@ -0,0 +1,6 @@
+var element = React.createElement(
+  "h1",
+  { className: "greeting" },
+  "Changed greeting"
+);
+ReactDOM.render(element, document.getElementById('root'));
---
+++ src/script.js
@@ -0,0 +1,5 @@
+const element = <h1 className="greeting">Changed greeting</h1>
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
